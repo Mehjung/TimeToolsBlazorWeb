a0a8ddc4e83a3b2e65d104b3b697860739f30c38
Mehjung <89900707+Mehjung@users.noreply.github.com>
2024-02-18 11:00:21 +0000

    Added and testet Dbimage.razor, Dbheadline.razor Components

--- TimeToolsBlazorWeb/wwwroot/js/componentInterop.js
+++ TimeToolsBlazorWeb/wwwroot/js/componentInterop.js
@@ -1,6 +1,5 @@
 window.setCheckboxIndeterminateState = function (customId, indeterminate) {
   const checkbox = document.querySelector(`[data-custom-id="${customId}"]`);
-  console.log("State changed to indeterminate: " + indeterminate + " for checkbox with customId: " + customId + " and checkbox: " + checkbox);
   if (checkbox) {
     checkbox.indeterminate = indeterminate;
   }
